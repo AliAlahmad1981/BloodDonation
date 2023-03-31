b0b0e27b4123f6ef069d3bda9fe0bcf07609c7b0
AliAlahmad1981 <ali.alabdalahmad@utb.ecutbildning.se>
2023-03-31 09:23:48 +0200

    adding inventory api

--- InventoryService/Program.cs
+++ InventoryService/Program.cs
@@ -1,8 +1,11 @@
 using InventoryService.Data;
+using InventoryService.Models;
 using InventoryService.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.Data;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -46,6 +49,66 @@ app.UseAuthentication();
 app.UseAuthorization();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<BloodInventoryService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+// Add Blood Type
+app.MapPost("/blood",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+async (Blood blood, InventoryDBContext db) =>
+    {
+        blood.Id = Guid.NewGuid();
+        await db.Bloods.AddAsync(blood);
+        await db.SaveChangesAsync();
+        return Results.Created($"/blood/{blood.Id}", "Blood Added  Successfully.");
+    });
+
+// Update Blood Type
+app.MapPut("/blood/{id}",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+async (string id, Blood blood, InventoryDBContext db) =>
+    {
+        if (Guid.TryParse(id, out var idGuid))
+        {
+            var bloodToUpdate = await db.Bloods.FindAsync(idGuid);
+            if (bloodToUpdate == null)
+                return Results.NotFound("blood Not Found.");
+            bloodToUpdate.BloodType = blood.BloodType;
+            bloodToUpdate.MinimumPacks = blood.MinimumPacks;
+            bloodToUpdate.MaximumPacks = blood.MaximumPacks;
+            await db.SaveChangesAsync();
+            return Results.Ok("blood Updated Successfully.");
+        }
+        else { return Results.BadRequest("Invalid blood Id"); }
+    });
+
+// Delete blood
+app.MapDelete("/blood/{id}",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+async (string id, InventoryDBContext db) =>
+    {
+        if (Guid.TryParse(id, out var idGuid))
+        {
+            var blood = await db.Bloods.FindAsync(idGuid);
+            if (blood == null)
+                return Results.NotFound("blood Not Found.");
+            db.Bloods.Remove(blood);
+            await db.SaveChangesAsync();
+            return Results.Ok("blood Removed Successfully.");
+        }
+        else { return Results.BadRequest("Invalid blood Id"); }
+    });
+
+// Find blood
+app.MapGet("/blood/{id}", async (string id, InventoryDBContext db) =>
+{
+    if (Guid.TryParse(id, out var idGuid))
+    {
+        var blood = await db.Bloods.FindAsync(idGuid);
+        return blood != null ? Results.Ok(blood) : Results.NotFound("blood Not Found.");
+    }
+    else { return Results.BadRequest("Invalid blood Id"); }
+});
+
+// Get bloods List
+app.MapGet("/bloods", async (InventoryDBContext db) => await db.Bloods.ToListAsync());
 
 app.Run();
+
