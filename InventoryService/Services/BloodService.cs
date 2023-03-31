89ba5d3f83194f59f78a0c9c486405af0f8f1c6c
AliAlahmad1981 <ali.alabdalahmad@utb.ecutbildning.se>
2023-03-31 09:25:36 +0200

    adding inventory grpc service

--- InventoryService/Services/BloodService.cs
+++ InventoryService/Services/BloodService.cs
@@ -1,22 +1,39 @@
 using Grpc.Core;
 using InventoryService;
+using InventoryService.Data;
+using InventoryService.Models;
 
 namespace InventoryService.Services
 {
     public class BloodInventoryService : BloodInventory.BloodInventoryBase
     {
         private readonly ILogger<BloodInventoryService> _logger;
-        public BloodInventoryService(ILogger<BloodInventoryService> logger)
+        private readonly InventoryDBContext _inventoryDBContext;
+
+        public BloodInventoryService(ILogger<BloodInventoryService> logger, InventoryDBContext inventoryDBContext)
         {
             _logger = logger;
+            _inventoryDBContext = inventoryDBContext;
         }
 
-        public override Task<AddBloodPackResponse> AddBloodPack(AddBloodPackRequest request, ServerCallContext context)
+        public override async Task<AddBloodPackResponse> AddBloodPack(AddBloodPackRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new AddBloodPackResponse
+            BloodPack bloodPack = new BloodPack()
             {
-                 Id = Guid.NewGuid().ToString()
-            });
+                Id = Guid.NewGuid(),
+                BloodId = new Guid(request.BloodId),
+                DonorId = new Guid(request.DonorId),
+                CollectionDate = DateOnly.FromDateTime(request.CollectionDate.ToDateTime()),
+                ExpirationDate = DateOnly.FromDateTime(request.ExpirationDate.ToDateTime())
+            };
+
+            await _inventoryDBContext.BloodPacks.AddAsync(bloodPack);
+            await _inventoryDBContext.SaveChangesAsync();
+            string stringId = bloodPack.Id.ToString();
+            return new AddBloodPackResponse
+            {
+                Id = stringId
+            };
         }
     }
 }
